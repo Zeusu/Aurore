742f85d917bc62b2b07cb57eb67997cf9bbba3b2
Zeusu <pierre.md.grenier@gmail.com>
2018-03-16 20:59:34 +0100

    Test

---
+++ src/net/aurore/command/utility/CommandPing.java
@@ -0,0 +1,43 @@
+package net.aurore.command.utility;
+
+import java.util.List;
+
+import net.aurore.command.AbstractCommandImpl;
+import net.aurore.command.Command;
+import net.aurore.command.CommandCat;
+import net.aurore.command.CommandContext;
+import net.aurore.command.CommandManagerImpl;
+import net.dv8tion.jda.core.entities.Member;
+
+@Command("ping")
+public class CommandPing extends AbstractCommandImpl {
+
+	private static final String PONG = "Pong !";
+	
+	public CommandPing(CommandManagerImpl manager) {
+		super(manager);
+	}
+
+	@Override
+	public void onInvoke(CommandContext context, String[] args, List<Member> mentioned) {
+		this.reply(context.getMsg(), PONG);
+
+	}
+
+	@Override
+	public String help() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCatHelp() {
+		return CommandCat.UTILITY.toString();
+	}
+
+	@Override
+	protected boolean isOk(CommandContext context, String[] args, List<Member> mentioned) {
+		return true;
+	}
+
+}
